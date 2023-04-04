1d92ab9e320b9c8425786b582badb77d8400b5cf
Athu5 <84505685+Athu5@users.noreply.github.com>
2023-04-04 17:53:18 +0530

    Update Feed.js

--- screens/Feed.js
+++ screens/Feed.js
@@ -33,12 +33,12 @@ return (
     <SafeAreaView style={styles.droidSafeArea} />
     <View style={styles.appTitle}>
     <View style={styles.appIcon}>
-    <Image source={require("../assets/logo.png")} style={{ width: 60,
-    height: 60, resizeMode: 'contain', marginLeft: 10 }}></Image>
+    <Image source={require("../assets/logo.png")} 
+           style={{ width: 60,height: 60, resizeMode: 'contain', marginLeft: 10 }}></Image>
     </View>
     <View style={styles.appTitleTextContainer}>
     <Text style={styles.appTitleText}>
-    Storytelling App
+    Spectagram
     </Text>
     </View>
     </View>
@@ -54,3 +54,38 @@ return (
         }
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "black"
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+    },
+    appTitle: {
+        flex: 0.07,
+        flexDirection: "row"
+    },
+    appIcon: {
+        flex: 0.2,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    iconImage: {
+        width: "100%",
+        height: "100%",
+        resizeMode: "contain"
+    },
+    appTitleTextContainer: {
+        flex: 0.8,
+        justifyContent: "center"
+    },
+    appTitleText: {
+        color: "white",
+        fontSize: RFValue(28),
+    },
+    cardContainer: {
+        flex: 0.85
+    }
+});
