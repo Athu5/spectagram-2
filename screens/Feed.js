403b9a124454a38c941f78a776eaf6ecb2dffec6
Athu5 <84505685+Athu5@users.noreply.github.com>
2023-04-04 18:00:19 +0530

    Update Feed.js

--- screens/Feed.js
+++ screens/Feed.js
@@ -1,60 +1,58 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    Platform,
+    StatusBar,
+    Image
+} from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
 import PostCard from "./PostCard";
-import AppLoading from 'expo-app-loading';
-import * as Font from 'expo-font';
 
-let customFonts = {
-'Bubblegum-Sans': require('../assets/fonts/BubblegumSans-Regular.ttf'),
-};
+import { FlatList } from "react-native-gesture-handler";
+
+let posts = require("./temp_posts.json");
 
 export default class Feed extends Component {
-constructor(props) {
-super(props);
-this.state = {
-fontsLoaded: false,
-        };
+    constructor(props) {
+        super(props);
+        this.state = {};
     }
-     renderItem = ({ item: post }) => {
-         return <PostCard post={post} navigation={this.props.navigation} />;
+
+    componentDidMount() { }
+
+    renderItem = ({ item: post }) => {
+        return <PostCard post={post} navigation={this.props.navigation} />;
     };
 
-    async _loadFontsAsync() {
-        await Font.loadAsync(customFonts);
-        this.setState({ fontsLoaded: true });
-        }
-        componentDidMount() {
-        this._loadFontsAsync();
-        }
-        render() {
-        if (!this.state.fontsLoaded) {
-        return <AppLoading />;
-        } else {
+    keyExtractor = (item, index) => index.toString();
 
-return (
-    <View style={styles.container}>
-    <SafeAreaView style={styles.droidSafeArea} />
-    <View style={styles.appTitle}>
-    <View style={styles.appIcon}>
-    <Image source={require("../assets/logo.png")} 
-           style={{ width: 60,height: 60, resizeMode: 'contain', marginLeft: 10 }}></Image>
-    </View>
-    <View style={styles.appTitleTextContainer}>
-    <Text style={styles.appTitleText}>
-    Spectagram
-    </Text>
-    </View>
-    </View>
-    <View style={styles.cardContainer}>
-    <FlatList
-    keyExtractor={this.keyExtractor}
-    data={stories}
-    renderItem={this.renderItem}
-    />
-    </View>
-    </View>
-    )
-        }
+    render() {
+        return (
+            <View style={styles.container}>
+                <SafeAreaView style={styles.droidSafeArea} />
+                <View style={styles.appTitle}>
+                    <View style={styles.appIcon}>
+                        <Image
+                            source={require("../assets/logo.png")}
+                            style={styles.iconImage}
+                        ></Image>
+                    </View>
+                    <View style={styles.appTitleTextContainer}>
+                        <Text style={styles.appTitleText}>Spectagram</Text>
+                    </View>
+                </View>
+                <View style={styles.cardContainer}>
+                    <FlatList
+                        keyExtractor={this.keyExtractor}
+                        data={posts}
+                        renderItem={this.renderItem}
+                    />
+                </View>
+            </View>
+        );
     }
 }
 
